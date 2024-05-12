a9851f2be09b0c2ed3eb6127319b5445cbd59faa
Neucrack <CZD666666@gmail.com>
2024-05-12 19:35:35 +0800

    fix lvgl compile error

--- components/3rd_party/lvgl/component.py
+++ components/3rd_party/lvgl/component.py
@@ -4,7 +4,7 @@ def add_file_downloads(confs : dict) -> list:
         @param confs kconfig vars, dict type
         @return list type, items is dict type
     '''
-    version = f"{confs['CONFIG_LVGL_VERSION_MAJOR']}.{confs['CONFIG_LVGL_VERSION_MAJOR']}.{confs['CONFIG_LVGL_VERSION_MAJOR']}"
+    version = f"{confs['CONFIG_LVGL_VERSION_MAJOR']}.{confs['CONFIG_LVGL_VERSION_MINOR']}.{confs['CONFIG_LVGL_VERSION_PATCH']}"
     url = f"https://github.com/lvgl/lvgl/archive/refs/tags/v{version}.tar.gz"
     if version == "9.1.0":
         sha256sum = "6930f1605d305fcd43f31d5f470ecf4a013c4ce0980e78ee4c33b96a589bf433"
