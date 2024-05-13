88c828079a79d69b20bfcba3fb9bdbce2aba6882
Neucrack <CZD666666@gmail.com>
2024-05-13 20:09:46 +0800

    add pinmap module and add pwm support

--- components/peripheral/include/maix_pinmap.hpp
+++ components/peripheral/include/maix_pinmap.hpp
@@ -1,7 +1,36 @@
 #pragma once
 
+#include "maix_basic.hpp"
 
+namespace maix::peripheral::pinmap
+{
 
+    /**
+     * Get all pins of devices
+     * @return pin name list, string type.
+     * @maixpy maix.peripheral.pinmap.get_pins
+    */
+    std::vector<std::string> get_pins();
+
+    /**
+     * Get all function of a pin
+     * @param pin pin name, string type.
+     * @return function list, function name is string type.
+     * @throw If pin name error will throwout err.Err.ERR_ARGS error.
+     * @maixpy maix.peripheral.pinmap.get_pin_functions
+    */
+    std::vector<std::string> get_pin_functions(const std::string &pin);
+
+    /**
+     * Set function of a pin
+     * @param pin pin name, string type.
+     * @param func which function should this pin use.
+     * @return if set ok, will return err.Err.ERR_NONE, else error occurs.
+     * @maixpy maix.peripheral.pinmap.set_pin_function
+    */
+    err::Err set_pin_function(const std::string &pin, const std::string &func);
+
+} // namespace maix::peripheral::pinmap
 
 
 
---
+++ examples/peripheral_pwm_servo/main/src/main.cpp
@@ -0,0 +1,61 @@
+
+#include "maix_basic.hpp"
+#include "maix_pwm.hpp"
+#include "main.hpp"
+#include "maix_pinmap.hpp"
+
+using namespace maix;
+using namespace maix::peripheral;
+
+
+#define SERVO_PERIOD    50   // 20ms
+#define SERVO_MIN_DUTY  2.5  // 2.5% -> 0.5ms
+#define SERVO_MAX_DUTY  12.5 // 12.5% -> 2.5ms
+
+float angle_to_duty(float percent)
+{
+    return (SERVO_MAX_DUTY - SERVO_MIN_DUTY) * percent / 100.0 + SERVO_MIN_DUTY;
+}
+
+int _main(int argc, char* argv[])
+{
+    int pwm_id = 7; // for MaixCAM PWM7 is pin A19, see https://wiki.sipeed.com/hardware/zh/maixcam/index.html
+
+    // set pinmap
+    pinmap::set_pin_function("A19", "PWM7");
+
+    // init pwm
+    pwm::PWM output(pwm_id, SERVO_PERIOD, angle_to_duty(0), true);
+
+    for(int i=0; i<100; ++i)
+    {
+        output.duty(angle_to_duty(i));
+        time::sleep_ms(100);
+    }
+
+    for(int i=100; i>=0; --i)
+    {
+        output.duty(angle_to_duty(i));
+        time::sleep_ms(100);
+    }
+
+    // while(!app::need_exit())
+    // {
+    //     time::sleep(1);
+    // }
+
+    return 0;
+}
+
+int main(int argc, char* argv[])
+{
+    // Catch SIGINT signal(e.g. Ctrl + C), and set exit flag to true.
+    signal(SIGINT, [](int sig){ app::set_exit_flag(true); });
+
+    // Use CATCH_EXCEPTION_RUN_RETURN to catch exception,
+    // if we don't catch exception, when program throw exception, the objects will not be destructed.
+    // So we catch exception here to let resources be released(call objects' destructor) before exit.
+    CATCH_EXCEPTION_RUN_RETURN(_main, -1, argc, argv);
+}
+
+
