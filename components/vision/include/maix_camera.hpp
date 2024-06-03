1750f2954fbe94b11fb01c8935d5183d659f48bf
lxowalle <lxowalle@outlook.com>
2024-06-03 17:46:21 +0800

    * add tips for camera

--- components/vision/include/maix_camera.hpp
+++ components/vision/include/maix_camera.hpp
@@ -46,7 +46,8 @@ namespace maix::camera
     {
     public:
         /**
-         * @brief Construct a new Camera object
+         * @brief Construct a new Camera object.
+         * The RGB888 format supports a maximum resolution of 640x640 and the FMT_YVU420SP format supports a maximum resolution of 2560x1440.
          * @param width camera width, default is -1, means auto, mostly means max width of camera support
          * @param height camera height, default is -1, means auto, mostly means max height of camera support
          * @param format camera output format, default is image.Format.FMT_RGB888
