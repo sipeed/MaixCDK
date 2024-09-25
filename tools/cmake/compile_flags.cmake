d00a489debddc5e627fba4e3d803acec8c42b9d8
Neucrack <CZD666666@gmail.com>
2024-09-25 15:59:07 +0800

    [important!] upgrade C++ compile version to C++20

--- tools/cmake/compile_flags.cmake
+++ tools/cmake/compile_flags.cmake
@@ -5,7 +5,7 @@ set(CMAKE_C_FLAGS ${CMAKE_C_FLAGS} -Wall -fPIC)
 
 
 ###### set CXX(cpp) flags ######
-set(CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} -Wall -fPIC)
+set(CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} -Wall -fPIC -std=c++2a)
 ################################
 
 # set(LINK_FLAGS -Wl,-EL) # (default little endian)
