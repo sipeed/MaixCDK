94d9eaeb5bb1d6d248e6dc74149b0f957f7208a8
lxowalle <lxowalle@outlook.com>
2024-12-16 10:20:20 +0800

    * update some examples

---
+++ examples/demo_focus_stack/main/src/main.cpp
@@ -0,0 +1,186 @@
+#include <iostream>
+#include "options.hh"
+#include "focusstack.hh"
+#include <opencv2/core.hpp>
+
+#ifndef GIT_VERSION
+#define GIT_VERSION "unknown"
+#endif
+
+using namespace focusstack;
+
+int main(int argc, const char *argv[])
+{
+  Options options(argc, argv);
+  FocusStack stack;
+
+  if (options.has_flag("--version"))
+  {
+    std::cerr << "focus-stack " GIT_VERSION ", built " __DATE__ " " __TIME__ "\n"
+                 "Compiled with OpenCV version " CV_VERSION "\n"
+                 "Copyright (c) 2019 Petteri Aimonen\n\n"
+
+"Permission is hereby granted, free of charge, to any person obtaining a copy\n"
+"of this software and associated documentation files (the \"Software\"), to\n"
+"deal in the Software without restriction, including without limitation the\n"
+"rights to use, copy, modify, merge, publish, distribute, sublicense, and/or\n"
+"sell copies of the Software, and to permit persons to whom the Software is\n"
+"furnished to do so, subject to the following conditions:\n\n"
+
+"The above copyright notice and this permission notice shall be included in all\n"
+"copies or substantial portions of the Software.\n\n"
+
+"THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n"
+"IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n"
+"FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n"
+"AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n"
+"LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n"
+"OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\n"
+"SOFTWARE."
+              << std::endl;
+    return 0;
+  }
+
+  if (options.has_flag("--opencv-version"))
+  {
+    std::cerr << cv::getBuildInformation().c_str() << std::endl;
+    return 0;
+  }
+
+  if (options.has_flag("--help") || options.get_filenames().size() < 2)
+  {
+    std::cerr << "Usage: " << argv[0] << " [options] file1.jpg file2.jpg ...\n";
+    std::cerr << "\n";
+    std::cerr << "Output file options:\n"
+                 "  --output=output.jpg           Set output filename\n"
+                 "  --depthmap=depthmap.png       Write a depth map image (default disabled)\n"
+                 "  --3dview=3dview.png           Write a 3D preview image (default disabled)\n"
+                 "  --save-steps                  Save intermediate images from processing steps\n"
+                 "  --jpgquality=95               Quality for saving in JPG format (0-100, default 95)\n"
+                 "  --nocrop                      Save full image, including extrapolated border data\n";
+    std::cerr << "\n";
+    std::cerr << "Image alignment options:\n"
+                 "  --reference=0                 Set index of image used as alignment reference (default middle one)\n"
+                 "  --global-align                Align directly against reference (default with neighbour image)\n"
+                 "  --full-resolution-align       Use full resolution images in alignment (default max 2048 px)\n"
+                 "  --no-whitebalance             Don't attempt to correct white balance differences\n"
+                 "  --no-contrast                 Don't attempt to correct contrast and exposure differences\n"
+                 "  --align-only                  Only align the input image stack and exit\n"
+                 "  --align-keep-size             Keep original image size by not cropping alignment borders\n";
+    std::cerr << "\n";
+    std::cerr << "Image merge options:\n"
+                 "  --consistency=2               Neighbour pixel consistency filter level 0..2 (default 2)\n"
+                 "  --denoise=1.0                 Merged image denoise level (default 1.0)\n";
+    std::cerr << "\n";
+    std::cerr << "Depth map generation options:\n"
+                 "  --depthmap-threshold=10       Threshold to accept depth points (0-255, default 10)\n"
+                 "  --depthmap-smooth-xy=20       Smoothing of depthmap in X and Y directions (default 20)\n"
+                 "  --depthmap-smooth-z=40        Smoothing of depthmap in Z direction (default 40)\n"
+                 "  --remove-bg=0                 Positive value removes black background, negative white\n"
+                 "  --halo-radius=20              Radius of halo effects to remove from depthmap\n"
+                 "  --3dviewpoint=x:y:z:zscale    Viewpoint for 3D view (default 1:1:1:2)\n";
+    std::cerr << "\n";
+    std::cerr << "Performance options:\n"
+                 "  --threads=2                   Select number of threads to use (default number of CPUs + 1)\n"
+                 "  --batchsize=8                 Images per merge batch (default 8)\n"
+                 "  --no-opencl                   Disable OpenCL GPU acceleration (default enabled)\n"
+                 "  --wait-images=0.0             Wait for image files to appear (allows simultaneous capture and processing)\n";
+    std::cerr << "\n";
+    std::cerr << "Information options:\n"
+                 "  --verbose                     Verbose output from steps\n"
+                 "  --version                     Show application version number\n"
+                 "  --opencv-version              Show OpenCV library version and build info\n";
+    return 1;
+  }
+
+  // Output file options
+  stack.set_inputs(options.get_filenames());
+  stack.set_output(options.get_arg("--output", "output.jpg"));
+  stack.set_depthmap(options.get_arg("--depthmap", ""));
+  stack.set_3dview(options.get_arg("--3dview", ""));
+  stack.set_jpgquality(std::stoi(options.get_arg("--jpgquality", "95")));
+  stack.set_save_steps(options.has_flag("--save-steps"));
+  stack.set_nocrop(options.has_flag("--nocrop"));
+
+  // Image alignment options
+  int flags = FocusStack::ALIGN_DEFAULT;
+  if (options.has_flag("--global-align"))             flags |= FocusStack::ALIGN_GLOBAL;
+  if (options.has_flag("--full-resolution-align"))    flags |= FocusStack::ALIGN_FULL_RESOLUTION;
+  if (options.has_flag("--no-whitebalance"))          flags |= FocusStack::ALIGN_NO_WHITEBALANCE;
+  if (options.has_flag("--no-contrast"))              flags |= FocusStack::ALIGN_NO_CONTRAST;
+  if (options.has_flag("--align-keep-size"))          flags |= FocusStack::ALIGN_KEEP_SIZE;
+  stack.set_align_flags(flags);
+
+  if (options.has_flag("--reference"))
+  {
+    stack.set_reference(std::stoi(options.get_arg("--reference")));
+  }
+
+  if (options.has_flag("--align-only"))
+  {
+    stack.set_align_only(true);
+    stack.set_output(options.get_arg("--output", "aligned_"));
+  }
+
+  // Image merge options
+  stack.set_consistency(std::stoi(options.get_arg("--consistency", "2")));
+  stack.set_denoise(std::stof(options.get_arg("--denoise", "1.0")));
+
+  // Depth map generation options
+  stack.set_depthmap_smooth_xy(std::stof(options.get_arg("--depthmap-smooth-xy", "20")));
+  stack.set_depthmap_smooth_z(std::stof(options.get_arg("--depthmap-smooth-z", "40")));
+  stack.set_depthmap_threshold(std::stoi(options.get_arg("--depthmap-threshold", "10")));
+  stack.set_halo_radius(std::stof(options.get_arg("--halo-radius", "20")));
+  stack.set_remove_bg(std::stoi(options.get_arg("--remove-bg", "0")));
+  stack.set_3dviewpoint(options.get_arg("--3dviewpoint", "1:1:1:2"));
+
+  // Performance options
+  if (options.has_flag("--threads"))
+  {
+    stack.set_threads(std::stoi(options.get_arg("--threads")));
+  }
+
+  if (options.has_flag("--batchsize"))
+  {
+    stack.set_batchsize(std::stoi(options.get_arg("--batchsize")));
+  }
+
+  stack.set_disable_opencl(options.has_flag("--no-opencl"));
+  stack.set_wait_images(std::stof(options.get_arg("--wait-images", "0.0")));
+
+  // Information options (some are handled at beginning of this function)
+  stack.set_verbose(options.has_flag("--verbose"));
+
+  // Check for any unhandled options
+  std::vector<std::string> unparsed = options.get_unparsed();
+  if (unparsed.size())
+  {
+    std::cerr << "Warning: unknown options: ";
+    for (std::string arg: unparsed)
+    {
+      std::cerr << arg << " ";
+    }
+    std::cerr << std::endl;
+  }
+
+
+  if (!stack.run())
+  {
+    std::printf("\nError exit due to failed steps\n");
+    return 1;
+  }
+
+  std::printf("\rSaved to %-40s\n", stack.get_output().c_str());
+
+  if (stack.get_depthmap() != "")
+  {
+    std::printf("\rSaved depthmap to %s\n", stack.get_depthmap().c_str());
+  }
+
+  if (stack.get_3dview() != "")
+  {
+    std::printf("\rSaved 3D preview to %s\n", stack.get_3dview().c_str());
+  }
+
+  return 0;
+}
---
+++ examples/onnxruntime_demo/main/core/bin/stream_kws_speechcommand.cc
@@ -0,0 +1,124 @@
+// Copyright (c) 2022 Zhendong Peng (pzd17@tsinghua.org.cn)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <signal.h>
+#include <iomanip>
+#include <iostream>
+#include <string>
+
+#include "portaudio.h"  // NOLINT
+
+#include "frontend/feature_pipeline.h"
+#include "frontend/wav.h"
+#include "kws/keyword_spotting.h"
+#include "utils/log.h"
+
+int g_exiting = 0;
+std::shared_ptr<wenet::FeaturePipeline> g_feature_pipeline;
+
+void SigRoutine(int dunno) {
+  if (dunno == SIGINT) {
+    g_exiting = 1;
+  }
+}
+
+static int RecordCallback(const void* input, void* output,
+                          unsigned long frames_count,  // NOLINT
+                          const PaStreamCallbackTimeInfo* time_info,
+                          PaStreamCallbackFlags status_flags, void* user_data) {
+  const auto* pcm_data = static_cast<const int16_t*>(input);
+  std::vector<int16_t> v(pcm_data, pcm_data + frames_count);
+  g_feature_pipeline->AcceptWaveform(v);
+
+  if (g_exiting) {
+    LOG(INFO) << "Exiting loop.";
+    g_feature_pipeline->set_input_finished();
+    return paComplete;
+  } else {
+    return paContinue;
+  }
+}
+
+static void check_result(std::vector<std::vector<float>> results)
+{
+  printf("result number:%ld", results.size());
+  static std::string name[11] = {"wow", "yes", "no", "up", "down", "left", "right", "on", "off", "stop", "go"};
+  for (auto &result: results) {
+    for (int i = 0; i < result.size(); i ++) {
+      printf("%s:%.5f ", name[i].c_str(), result[i]);
+    }
+    printf("\r\n");
+  }
+}
+
+int main(int argc, char* argv[]) {
+  if (argc != 4) {
+    LOG(FATAL) << "Usage: kws_main fbank_dim batch_size kws_model_path";
+  }
+  const int num_bins = std::stoi(argv[1]);  // Fbank feature dim
+  const int batch_size = std::stoi(argv[2]);
+  const std::string model_path = argv[3];
+
+  wenet::FeaturePipelineConfig feature_config(num_bins, 16000);
+  g_feature_pipeline = std::make_shared<wenet::FeaturePipeline>(feature_config);
+  wekws::KeywordSpotting spotter(model_path);
+
+  signal(SIGINT, SigRoutine);
+  PaError err = Pa_Initialize();
+  PaStreamParameters params;
+  std::cout << err << " " << Pa_GetDeviceCount() << std::endl;
+  params.device = Pa_GetDefaultInputDevice();
+  if (params.device == paNoDevice) {
+    LOG(FATAL) << "Error: No default input device.";
+  }
+  params.channelCount = 1;
+  params.sampleFormat = paInt16;
+  params.suggestedLatency =
+      Pa_GetDeviceInfo(params.device)->defaultLowInputLatency;
+  params.hostApiSpecificStreamInfo = NULL;
+  PaStream* stream;
+  // Callback and spot pcm date each `interval` ms.
+  int interval = 500;
+  int frames_per_buffer = 16000 / 1000 * interval;
+  Pa_OpenStream(&stream, &params, NULL, 16000, frames_per_buffer, paClipOff,
+                RecordCallback, NULL);
+  Pa_StartStream(stream);
+  LOG(INFO) << "=== Now recording!! Please speak into the microphone. ===";
+
+  std::cout << std::setiosflags(std::ios::fixed) << std::setprecision(2);
+  printf(" =============== [%s][%d]\r\n", __func__, __LINE__);
+  while (Pa_IsStreamActive(stream)) {printf(" =============== [%s][%d]\r\n", __func__, __LINE__);
+    Pa_Sleep(interval);printf(" =============== [%s][%d]\r\n", __func__, __LINE__);
+    std::vector<std::vector<float>> feats;printf(" =============== [%s][%d]\r\n", __func__, __LINE__);
+    g_feature_pipeline->Read(batch_size, &feats);printf(" =============== [%s][%d]\r\n", __func__, __LINE__);
+    std::vector<std::vector<float>> prob;printf(" =============== [%s][%d]\r\n", __func__, __LINE__);
+    spotter.Forward(feats, &prob);printf(" =============== [%s][%d]\r\n", __func__, __LINE__);
+
+    check_result(prob);
+    // for (int t = 0; t < prob.size(); t++) {
+    //   for (int i = 0; i < prob[t].size(); i++) {
+    //     if (prob[t][i] > 0.001) {
+    //       printf("kw[%d][%d] %f\r\n", t, i, prob[t][i]);
+    //       // std::cout << " kw[" << i << "] " << prob[t][i];
+    //     }
+
+    //   }
+    //   // std::cout << std::endl;
+    // }
+  }
+  Pa_CloseStream(stream);
+  Pa_Terminate();
+
+  return 0;
+}
