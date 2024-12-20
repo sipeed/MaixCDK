ec9a6c46054ae9157d87cff0700680fb9c627d3a
Neucrack <CZD666666@gmail.com>
2024-12-20 11:23:07 +0800

    add YOLOv8/YOLO11 OBB support

--- components/nn/include/maix_nn_object.hpp
+++ components/nn/include/maix_nn_object.hpp
@@ -31,8 +31,8 @@ namespace maix::nn
          * @maixpy maix.nn.Object.__init__
          * @maixcdk maix.nn.Object.Object
          */
-        Object(int x = 0, int y = 0, int w = 0, int h = 0, int class_id = 0, float score = 0, std::vector<int> points = std::vector<int>())
-            : x(x), y(y), w(w), h(h), class_id(class_id), score(score), points(points), seg_mask(NULL), temp(NULL)
+        Object(int x = 0, int y = 0, int w = 0, int h = 0, int class_id = 0, float score = 0, std::vector<int> points = std::vector<int>(), float angle = -9999)
+            : x(x), y(y), w(w), h(h), class_id(class_id), score(score), points(points), angle(angle), seg_mask(NULL), temp(NULL)
         {
         }
 
@@ -48,7 +48,56 @@ namespace maix::nn
          */
         std::string to_str()
         {
-            return "x: " + std::to_string(x) + ", y: " + std::to_string(y) + ", w: " + std::to_string(w) + ", h: " + std::to_string(h) + ", class_id: " + std::to_string(class_id) + ", score: " + std::to_string(score);
+            if(angle != -9999)
+                return "x: " + std::to_string(x) + ", y: " + std::to_string(y) + ", w: " + std::to_string(w) + ", h: " + std::to_string(h) + ", class_id: " + std::to_string(class_id) + ", score: " + std::to_string(score) + ", angle: " + std::to_string(angle);
+            else
+                return "x: " + std::to_string(x) + ", y: " + std::to_string(y) + ", w: " + std::to_string(w) + ", h: " + std::to_string(h) + ", class_id: " + std::to_string(class_id) + ", score: " + std::to_string(score);
+        }
+
+        /**
+         * Get OBB(oriented bounding box) points, auto calculated according to x,y,w,h,angle
+         * @maixpy maix.nn.Object.get_obb_points
+         */
+        std::vector<int> get_obb_points()
+        {
+            // x, y, w, h, angle to x1,y1,x2,y2,x3,y3,x4,y4 list
+            std::vector<int> obb_points;
+            if(angle == -9999)
+            {
+                obb_points.push_back(x);
+                obb_points.push_back(y);
+                obb_points.push_back(x + w);
+                obb_points.push_back(y);
+                obb_points.push_back(x + w);
+                obb_points.push_back(y + h);
+                obb_points.push_back(x);
+                obb_points.push_back(y + h);
+            }
+            else
+            {
+                float angle_rad = angle * M_PI;
+                float c = cosf(angle_rad);
+                float s = sinf(angle_rad);
+                int x1 = x;
+                int y1 = y;
+                int x2 = x + w;
+                int y2 = y;
+                int x3 = x + w;
+                int y3 = y + h;
+                int x4 = x;
+                int y4 = y + h;
+                int cx = x + w / 2;
+                int cy = y + h / 2;
+                obb_points.push_back((int)(c * (x1 - cx) - s * (y1 - cy) + cx));
+                obb_points.push_back((int)(s * (x1 - cx) + c * (y1 - cy) + cy));
+                obb_points.push_back((int)(c * (x2 - cx) - s * (y2 - cy) + cx));
+                obb_points.push_back((int)(s * (x2 - cx) + c * (y2 - cy) + cy));
+                obb_points.push_back((int)(c * (x3 - cx) - s * (y3 - cy) + cx));
+                obb_points.push_back((int)(s * (x3 - cx) + c * (y3 - cy) + cy));
+                obb_points.push_back((int)(c * (x4 - cx) - s * (y4 - cy) + cx));
+                obb_points.push_back((int)(s * (x4 - cx) + c * (y4 - cy) + cy));
+            }
+            return obb_points;
         }
 
         /**
@@ -94,6 +143,12 @@ namespace maix::nn
         std::vector<int> points;
 
         /**
+         * Rotate angle, -9999 means not set, value is a percentage, need to multiply 180 to get the real angle or multiply PI to get the radian.
+         * @maixpy maix.nn.Object.angle
+         */
+        float angle;
+
+        /**
          * segmentation mask, uint8 list type, shape is h * w but flattened to one dimension, value fron 0 to 255.
          * @attention For efficiency, it's a pointer in C++, use this carefully!
          * @maixpy maix.nn.Object.seg_mask
@@ -124,8 +179,8 @@ namespace maix::nn
          * @maixpy maix.nn.ObjectFloat.__init__
          * @maixcdk maix.nn.ObjectFloat.ObjectFloat
          */
-        ObjectFloat(float x = 0, float y = 0, float w = 0, float h = 0, float class_id = 0, float score = 0, std::vector<float> points = std::vector<float>())
-            : x(x), y(y), w(w), h(h), class_id(class_id), score(score), points(points), temp(NULL)
+        ObjectFloat(float x = 0, float y = 0, float w = 0, float h = 0, float class_id = 0, float score = 0, std::vector<float> points = std::vector<float>(), float angle = -1)
+            : x(x), y(y), w(w), h(h), class_id(class_id), score(score), points(points), angle(angle), temp(NULL)
         {
         }
 
@@ -141,7 +196,10 @@ namespace maix::nn
          */
         std::string to_str()
         {
-            return "x: " + std::to_string(x) + ", y: " + std::to_string(y) + ", w: " + std::to_string(w) + ", h: " + std::to_string(h) + ", class_id: " + std::to_string(class_id) + ", score: " + std::to_string(score);
+            if(angle >= 0)
+                return "x: " + std::to_string(x) + ", y: " + std::to_string(y) + ", w: " + std::to_string(w) + ", h: " + std::to_string(h) + ", class_id: " + std::to_string(class_id) + ", score: " + std::to_string(score) + ", angle: " + std::to_string(angle);
+            else
+                return "x: " + std::to_string(x) + ", y: " + std::to_string(y) + ", w: " + std::to_string(w) + ", h: " + std::to_string(h) + ", class_id: " + std::to_string(class_id) + ", score: " + std::to_string(score);
         }
 
         /**
@@ -187,6 +245,12 @@ namespace maix::nn
         std::vector<float> points;
 
         /**
+         * Rotate angle
+         * @maixpy maix.nn.ObjectFloat.angle
+        */
+        float angle;
+
+        /**
          * For temperary usage, not for MaixPy API
          */
         void *temp;
@@ -226,9 +290,9 @@ namespace maix::nn
          * @throw Throw exception if no memory
          * @maixpy maix.nn.Objects.add
          */
-        nn::Object &add(int x = 0, int y = 0, int w = 0, int h = 0, int class_id = 0, float score = 0, std::vector<int> points = std::vector<int>())
+        nn::Object &add(int x = 0, int y = 0, int w = 0, int h = 0, int class_id = 0, float score = 0, std::vector<int> points = std::vector<int>(), float angle = -1)
         {
-            Object *obj = new Object(x, y, w, h, class_id, score, points);
+            Object *obj = new Object(x, y, w, h, class_id, score, points, angle);
             if(!obj)
                 throw err::Exception(err::ERR_NO_MEM);
             obj->seg_mask = NULL;
