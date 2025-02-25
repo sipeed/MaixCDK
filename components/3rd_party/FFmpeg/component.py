15f67bde3ce491e509fbd8f6a760fe61a359766a
lxowalle <lxowalle@outlook.com>
2025-02-25 16:18:38 +0800

    * fix compile error

--- components/3rd_party/FFmpeg/component.py
+++ components/3rd_party/FFmpeg/component.py
@@ -9,6 +9,8 @@ def add_file_downloads(confs : dict) -> list:
         url = f"https://github.com/sipeed/MaixCDK/releases/download/v0.0.0/ffmpeg_libs_n{version}.tar.xz"
         if version == "4.4.4.1":
             sha256sum = "d4b8eb70a7864c855e8e286fef39b9b90faf72c1b77491148c451da252cad6c0"
+        elif version == "4.4.4.2":
+            sha256sum = "aa95faf66e92f9b244e5d48006ceee1a6e3cfb1122fe78f9fbe3a0cf6db7a7f2"
         else:
             raise Exception(f"version {version} not support")
         sites = ["https://github.com/sipeed/MaixCDK/releases/tag/v0.0.0"]
