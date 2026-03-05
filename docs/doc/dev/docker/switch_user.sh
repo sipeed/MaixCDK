0f4cc11daeba090c44571afef1c019caf8ed07ed
compile-js <15362305666@189.cn>
2026-03-05 11:35:43 +0800

    增加了MaixPy的编译环境

--- docs/doc/dev/docker/switch_user.sh
+++ docs/doc/dev/docker/switch_user.sh
@@ -2,44 +2,6 @@
 
 set -e
 
-UID=${UID:-""}
-GID=${GID:-""}
-USER=${USER:-""}
-
-if [ -z "$USER" ]; then
-    echo "USER env is required by --env USER=\$USER"
-    exit 1
-fi
-
-if [ -z "$UID" ]; then
-    echo "UID env is required by --env UID=\`id -u\`"
-    exit 1
-fi
-
-if [ -z "$GID" ]; then
-    echo "GID env is required by --env GID=\`id -g\`"
-    exit 1
-fi
-
-# if group $USER not exists, add group
-if ! grep -q "^$USER:" /etc/group; then
-    addgroup --gid "$GID" "$USER"
-fi
-
-# if user $UID not exists, add user
-if ! grep -q "^$USER:" /etc/passwd; then
-    adduser --gecos "" --uid "$UID" --gid "$GID" --no-create-home --disabled-password "$USER"
-    mkdir -p /home/"$USER"
-    cp -r /etc/skel/. /home/"$USER"
-    # default password is maixcdk
-    echo "$USER:maixcdk" | chpasswd
-    usermod -aG sudo "$USER"
-fi
-chown "$UID":"$GID" /home/"$USER"
-
-# add /maixapp directory and chown to $USER
-mkdir -p /maixapp
-chown "$UID":"$GID" /maixapp
 
 echo "###########################################################"
 echo "##"
@@ -61,12 +23,8 @@ else
     echo "## Please set MAIXCDK_PATH to MaixCDK path in container"
     echo "## e.g. add args \`--env MAIXCDK_PATH=/path/to/MaixCDK -v /path/to/MaixCDK:/path/to/MaixCDK\` to docker run command"
 fi
-echo "## The default password of user ${USER} is 'maixcdk'"
+# echo "## The default password of user ${USER} is 'maixcdk'"
 echo "###########################################################"
 echo ""
 
-cd /home/"$USER"
-
-su "$USER"
-
-
+bash 
